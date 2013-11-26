ea8e703651e083f361c6b0a637dc2f8f0a56be94
Phil Gyford <phil@gyford.com>
2013-11-26 16:04:43 +0000

    Improve db, oauth accessor functions

--- publication.py
+++ publication.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 from datetime import datetime
 import dateutil.parser
-from flask import Flask, abort, json, jsonify, make_response, render_template, Response, request, send_from_directory
+from flask import Flask, abort, g, json, jsonify, make_response, render_template, Response, request, send_from_directory
 import hashlib
 import oauth2 as oauth
 import os
@@ -42,26 +42,48 @@ except IOError as e:
     app.config['REDIS_URL'] = os.environ.get('REDIS_URL', False)
 
 
+# The BERG Cloud OAuth consumer object.
 def consumer():
-    return oauth.Consumer(
-            key=app.config['BERGCLOUD_CONSUMER_TOKEN'], 
-            secret=app.config['BERGCLOUD_CONSUMER_TOKEN_SECRET'])
+    consumer = getattr(g, '_oauth_consumer', None)
+    if consumer is None:
+        consumer = g._oauth_consumer = oauth.Consumer(
+                        key=app.config['BERGCLOUD_CONSUMER_TOKEN'], 
+                        secret=app.config['BERGCLOUD_CONSUMER_TOKEN_SECRET'])
+    return consumer
 
+# The BERG Cloud OAuth access token.
 def access_token():
-    return oauth.Token(
-            key=app.config['BERGCLOUD_ACCESS_TOKEN'],
-            secret=app.config['BERGCLOUD_ACCESS_TOKEN_SECRET'])
+    token = getattr(g, '_oauth_access_token', None)
+    if token is None:
+        token = g._oauth_access_token = oauth.Token(
+                            key=app.config['BERGCLOUD_ACCESS_TOKEN'],
+                            secret=app.config['BERGCLOUD_ACCESS_TOKEN_SECRET'])
+    return token
 
+# The BERG Cloud OAuth client.
+# Use something like:
+#   response, data = client().request(url, 
+#                       method='POST', body='Hello',
+#                       headers={'Content-Type': 'text/html; charset=utf-8'})
 def client():
-    return oauth.Client(consumer(), access_token())
+    client = getattr(g, '_oauth_client', None)
+    if client is None:
+        client = g._oauth_client = oauth.Client(consumer(), access_token())
+    return client
 
 
+# Returns the Redis object (either new or existing).
 def db():
-    if 'REDIS_URL' in app.config and app.config['REDIS_URL']:
-        url = urlparse.urlparse(app.config['REDIS_URL'])
-        return redis.Redis(host=url.hostname, port=url.port, password=url.password)
-    else:
-        return redis.Redis()
+    db = getattr(g, '_database', None)
+    if db is None:
+        if 'REDIS_URL' in app.config and app.config['REDIS_URL']:
+            # If there's a REDIS_URL config variable, connect with that.
+            url = urlparse.urlparse(app.config['REDIS_URL'])
+            db = g._database = redis.Redis(host=url.hostname, port=url.port, password=url.password)
+        else:
+            # Otherwise, use local Redis.
+            db = g._database = redis.Redis()
+    return db
 
 
 @app.route('/')
